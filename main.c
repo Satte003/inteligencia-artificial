dcbeec386efa7b303b667919ef5d723f6cd84c23
Emma79662 <emmagopro96@gmail.com>
2024-11-12 20:23:43 +0100

    avances en el main

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+//ver cuales hacen falta
+#include "cancion.h"
+#include "cargarDatos.h"
+#include "esExito.h"
+#include "listaCancion.h"
+#include "normalizar.h"
+
+//función para usar el knn
 knn(datos, elem);
+
+int main(){
+    char respuesta;
+
+    do{
+        //programa
+
+        printf("¿Quieres repetir la ejecución? (s/n)\n");
+        scanf(" %c", &respuesta);
+    } while(respuesta = "s");
+
+    return 0;
+}
